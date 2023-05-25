a54e9d1a43202e1998db138eb50c97b89f3c9ae7
Letizia <116899981+letizia-sorrentino@users.noreply.github.com>
2023-05-25 11:30:17 +0100

    add files

---
+++ src/Gallery.jsx
@@ -0,0 +1,17 @@
+import Profile from "./Profile.jsx";
+
+const Gallery = () => {
+
+    return (
+  <section>
+        <h1>Dogs</h1>
+        <Profile />
+        <Profile />
+        <Profile />
+      </section>
+    );
+  }
+  
+  export default Gallery;
+  
+  
---
+++ src/Profile.jsx
@@ -0,0 +1,7 @@
+const Profile = () => {
+  return (
+    <img src="./week1-homework/src/assets/images/booboo.jpg" alt="booboo" />
+  );
+};
+
+export default Profile;
